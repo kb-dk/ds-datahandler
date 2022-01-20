9810d999bb681e8c6b0a5d7fe0d524a8739a4a38
Thomas Egense <teg@kb.dk>
2022-01-20 13:45:11 +0100

    dsstorage client generation... not working

---
+++ src/test/java/dk/kb/datahandler/storageclient/OaiHarvestClientTest.java
@@ -0,0 +1,23 @@
+package dk.kb.datahandler.oai;
+
+import org.junit.jupiter.api.Test;
+
+import dk.kb.storageclient.invoker.ApiClient;
+
+/*
+ * This is an integration test that must be run manually.
+ * 
+ */
+public class OaiHarvestClientTest {
+
+    
+    public static void main(String[] args) throws Exception {
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath("http://devel11:10001/ds-storage/api/");
+
+        
+  //Det er vist ikke den klasse... 
+       
+    }
+    
+}
