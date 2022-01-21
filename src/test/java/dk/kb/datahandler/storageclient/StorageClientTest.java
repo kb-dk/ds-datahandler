9e6556751d437b166a07cfca66ab7a79ccbed4a7
Asger Askov Blekinge <abr@kb.dk>
2022-01-21 12:57:19 +0100

    Fixed client generation

--- src/test/java/dk/kb/datahandler/storageclient/StorageClientTest.java
+++ src/test/java/dk/kb/datahandler/storageclient/StorageClientTest.java
@@ -1,18 +1,31 @@
 package dk.kb.datahandler.storageclient;
 
-import org.junit.jupiter.api.Test;
+import dk.kb.datahandler.backend.api.v1.DsDatahandlerApi;
+import dk.kb.datahandler.backend.invoker.v1.ApiClient;
+import dk.kb.datahandler.backend.model.v1.RecordBaseDto;
 
+import java.util.List;
 
 
 /*
  * This is an integration test that must be run manually.
- * 
+ *
  */
 public class StorageClientTest{
 
     
     public static void main(String[] args) throws Exception {
-          
+        //Få main metoden i StorageClientTest til at lave et rigtigt kald.  Devel server API kører her: http://devel11:10001/ds-storage/api/
+        //Følgende metode på storage er simpel og kræver ingen parametre:
+        //http://devel11:10001/ds-storage/v1/getBasesConfiguration
+    
+        ApiClient apiClient = new ApiClient();
+        apiClient.setHost("devel11.statsbiblioteket.dk");
+        apiClient.setPort(10001);
+        apiClient.setBasePath("/ds-storage/v1");
+        DsDatahandlerApi dsAPI = new DsDatahandlerApi(apiClient);
+        List<RecordBaseDto> basesConf = dsAPI.getBasesConfiguration();
+        System.out.println(basesConf);
     }
     
 }
