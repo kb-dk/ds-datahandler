e0ead87ecf81c67395486d0df0a3f89f879ececa
Thomas Egense <teg@kb.dk>
2022-02-24 15:16:54 +0100

    More API integrationtest

--- src/test/java/dk/kb/datahandler/storageclient/StorageClientIntegrationTest.java
+++ src/test/java/dk/kb/datahandler/storageclient/StorageClientIntegrationTest.java
@@ -2,6 +2,7 @@ package dk.kb.datahandler.storageclient;
 
 import dk.kb.datahandler.backend.api.v1.DsStorageApi;
 import dk.kb.datahandler.backend.invoker.v1.ApiClient;
+import dk.kb.datahandler.backend.model.v1.DsRecordDto;
 import dk.kb.datahandler.backend.model.v1.RecordBaseDto;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class StorageClientIntegrationTest{
         //Følgende metode på storage er simpel og kræver ingen parametre:
         //http://devel11:10001/ds-storage/v1/getBasesConfiguration
     
+        /*
         ApiClient apiClient = new ApiClient();
         apiClient.setHost("devel11.statsbiblioteket.dk");
         apiClient.setPort(10001);
@@ -26,6 +28,18 @@ public class StorageClientIntegrationTest{
         DsStorageApi dsAPI = new DsStorageApi(apiClient);
         List<RecordBaseDto> basesConf = dsAPI.getBasesConfiguration();        
         System.out.println(basesConf);
+        */
+        
+        ApiClient apiClient = new ApiClient();
+        apiClient.setHost("devel11.statsbiblioteket.dk");
+        apiClient.setPort(10001);
+        apiClient.setBasePath("/ds-storage/v1");
+        DsStorageApi dsAPI = new DsStorageApi(apiClient);
+        DsRecordDto record = dsAPI.getRecord("kb.image.luftfo.luftfoto:oai:kb.dk:images:luftfo:2011:maj:luftfoto:object187744");        
+        System.out.println(record);
+        
+        
+        
     }
     
 }
