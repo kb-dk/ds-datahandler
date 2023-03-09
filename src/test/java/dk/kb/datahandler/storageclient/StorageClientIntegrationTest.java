521d0b8f7a77aad5a6eea8fb0bcfb56e5c2db3da
Thomas Egense <teg@kb.dk>
2023-03-09 14:05:38 +0100

    Switched to ds-storage autogenerated client dependency

--- src/test/java/dk/kb/datahandler/storageclient/StorageClientIntegrationTest.java
+++ src/test/java/dk/kb/datahandler/storageclient/StorageClientIntegrationTest.java
@@ -1,11 +1,7 @@
 package dk.kb.datahandler.storageclient;
 
-import dk.kb.datahandler.backend.api.v1.DsStorageApi;
-import dk.kb.datahandler.backend.invoker.v1.ApiClient;
-import dk.kb.datahandler.backend.model.v1.DsRecordDto;
-import dk.kb.datahandler.backend.model.v1.RecordBaseDto;
-
-import java.util.List;
+import dk.kb.storage.model.v1.DsRecordDto;
+import dk.kb.storage.util.DsStorageClient;
 
 
 /*
@@ -16,29 +12,14 @@ public class StorageClientIntegrationTest{
 
     
     public static void main(String[] args) throws Exception {
-        //Få main metoden i StorageClientTest til at lave et rigtigt kald.  Devel server API kører her: http://devel11:10001/ds-storage/api/
-        //Følgende metode på storage er simpel og kræver ingen parametre:
-        //http://devel11:10001/ds-storage/v1/getBasesConfiguration
-    
-        /*
-        ApiClient apiClient = new ApiClient();
-        apiClient.setHost("devel11.statsbiblioteket.dk");
-        apiClient.setPort(10001);
-        apiClient.setBasePath("/ds-storage/v1");
-        DsStorageApi dsAPI = new DsStorageApi(apiClient);
-        List<RecordBaseDto> basesConf = dsAPI.getBasesConfiguration();        
-        System.out.println(basesConf);
-        */
-        
-        ApiClient apiClient = new ApiClient();
-        apiClient.setHost("devel11.statsbiblioteket.dk");
-        apiClient.setPort(10001);
-        apiClient.setBasePath("/ds-storage/v1");
-        DsStorageApi dsAPI = new DsStorageApi(apiClient);
-        DsRecordDto record = dsAPI.getRecord("kb.image.luftfo.luftfoto:oai:kb.dk:images:luftfo:2011:maj:luftfoto:object187744");        
+           	
+    	//replace server with a running server with ds-storage installed
+    	String backEndUrl="http://server:10001/ds-storage/v1/";
+    	
+        DsStorageClient client = new DsStorageClient(backEndUrl);            
+     
+        DsRecordDto record = client.getRecord("kb.image.luftfo.luftfoto:oai:kb.dk:images:luftfo:2011:maj:luftfoto:object187744");        
         System.out.println(record);
-        
-        
         
     }
     
