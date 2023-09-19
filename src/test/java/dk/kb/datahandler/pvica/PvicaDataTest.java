69262644c7fbf0ca945272f8555a5b36b942d16d
Victor Harbo Johnston <vhol@kb.dk>
2023-09-19 12:19:27 +0200

    Update namespace replace to handle manifestations as well

--- src/test/java/dk/kb/datahandler/pvica/PvicaDataTest.java
+++ src/test/java/dk/kb/datahandler/pvica/PvicaDataTest.java
@@ -8,18 +8,26 @@ import org.junit.jupiter.api.Test;
 import dk.kb.datahandler.oai.OaiHarvestClient;
 import dk.kb.util.Resolver;
 
-import java.io.IOException;
-
 public class PvicaDataTest {
     
     @Test
-    public void testNameSpaceFix() throws Exception{
-        String xmlFile = "xml/pvica_namespace_tofix.xml";        
+    public void testDeliverableUnitNameSpaceFix() throws Exception{
+        String xmlFile = "xml/pvica_deliverableunit_namespace_tofix.xml";
         String xml = Resolver.resolveUTF8String(xmlFile);        
 
-        String xmlFixed= OaiHarvestClient.nameFixPvica(xml);          
-        assertTrue(xmlFixed.indexOf("<xip:DeliverableUnit xmlns:xip=\"http://www.tessella.com/XIP/v4\"") > 0);               
+        String xmlFixed= OaiHarvestClient.nameFixPvica(xml);
+        assertTrue(xmlFixed.indexOf("<xip:DeliverableUnit xmlns:xip=\"http://www.tessella.com/XIP/v4\"") > 0);
         assertTrue(xmlFixed.indexOf("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"") > 0);            
+    }
+
+
+    @Test
+    public void testManifestationNameSpaceFix() throws Exception{
+        String xmlFile = "xml/pvica_manifestation_namespace_to_fix.xml";
+        String xml = Resolver.resolveUTF8String(xmlFile);
+
+        String xmlFixed= OaiHarvestClient.nameFixPvica(xml);
+        assertTrue(xmlFixed.indexOf("<xip:Manifestation xmlns:xip=\"http://www.tessella.com/XIP/v4\"") > 0);
     }
 
 
