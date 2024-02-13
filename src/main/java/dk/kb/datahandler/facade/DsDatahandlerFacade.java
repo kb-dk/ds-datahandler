38407df559ee9e05e5cef029fe89ddd52a5e1759
Thomas Egense <teg@kb.dk>
2024-02-13 13:38:52 +0100

    code fix

--- src/main/java/dk/kb/datahandler/facade/DsDatahandlerFacade.java
+++ src/main/java/dk/kb/datahandler/facade/DsDatahandlerFacade.java
@@ -237,7 +237,7 @@ public class DsDatahandlerFacade {
        		return delta;    		
        	}
        	else {
-       		Integer delta = oaiIngestFull(oaiTargetName);	
+       		Integer delta = oaiIngestFullImpl(oaiTargetName);	
        		return delta;
        	}    	
     	
