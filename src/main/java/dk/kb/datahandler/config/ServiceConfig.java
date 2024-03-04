0f3b956c26a98c0c374156a6402f5b585fb12bd4
Victor Harbo Johnston <vhol@kb.dk>
2024-03-04 10:36:05 +0100

    Add default value in serviceconfig. Jetty doesnt work without

--- src/main/java/dk/kb/datahandler/config/ServiceConfig.java
+++ src/main/java/dk/kb/datahandler/config/ServiceConfig.java
@@ -128,7 +128,7 @@ public class ServiceConfig {
             String filterStr = target.getString("filter","direct");
             Boolean dayOnly = target.getBoolean("dayOnly",Boolean.FALSE);
             String startDay = target.getString("startDay",null);            
-            String dateStampFormat = target.getString("dateStampFormat");           
+            String dateStampFormat = target.getString("dateStampFormat", null);
             if (dayOnly) { //startDay must be defined for dayOnly strategy                        	      
                 boolean validStartDay=HarvestTimeUtil.validateDayFormat(startDay);
                 if (!validStartDay) {
