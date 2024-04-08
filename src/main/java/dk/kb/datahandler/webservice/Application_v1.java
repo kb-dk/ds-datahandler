52198d5183b8930047696d608a425fecaaebb889
Victor Harbo Johnston <vhol@kb.dk>
2024-04-08 12:26:59 +0200

    Update OpenApiResource usage

--- src/main/java/dk/kb/datahandler/webservice/Application_v1.java
+++ src/main/java/dk/kb/datahandler/webservice/Application_v1.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import dk.kb.datahandler.api.v1.impl.DsDatahandlerApiServiceImpl;
 import dk.kb.datahandler.api.v1.impl.ServiceApiServiceImpl;
+import dk.kb.datahandler.config.ServiceConfig;
+import dk.kb.util.webservice.OpenApiResource;
 import dk.kb.util.webservice.exception.ServiceExceptionMapper;
 
 
@@ -14,6 +16,8 @@ public class Application_v1 extends javax.ws.rs.core.Application {
 
     @Override
     public Set<Class<?>> getClasses() {
+        OpenApiResource.setConfig(ServiceConfig.getConfig());
+
         return new HashSet<>(Arrays.asList(
                 JacksonJsonProvider.class,
                 DsDatahandlerApiServiceImpl.class,
