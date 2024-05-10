0894e0cb86a51826e779166033fee12c27999111
Victor Harbo Johnston <vhol@kb.dk>
2024-05-10 11:05:46 +0200

    Add simple log to  boilerplate implementation

--- src/main/java/dk/kb/datahandler/oai/plugins/PreservicaManifestationPlugin.java
+++ src/main/java/dk/kb/datahandler/oai/plugins/PreservicaManifestationPlugin.java
@@ -1,8 +1,13 @@
 package dk.kb.datahandler.oai.plugins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PreservicaManifestationPlugin  implements Plugin{
+    private static final Logger log = LoggerFactory.getLogger(PreservicaManifestationPlugin.class);
+
     @Override
     public void apply() {
-
+        log.info("Preservica Manifestation Plugin has been applied.");
     }
 }
