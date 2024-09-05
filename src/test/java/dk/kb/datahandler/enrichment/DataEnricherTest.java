6cf2faee683f0b26aafa2384e40b6443a04218e9
David Grove Jørgensen <dgj@kb.dk>
2024-09-05 16:47:44 +0200

    DRA-1203 added unittests

--- src/test/java/dk/kb/datahandler/enrichment/DataEnricherTest.java
+++ src/test/java/dk/kb/datahandler/enrichment/DataEnricherTest.java
@@ -3,7 +3,7 @@ package dk.kb.datahandler.enrichment;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.kb.datahandler.config.ServiceConfig;
-import dk.kb.datahandler.oai.OaiRecord;
+import dk.kb.datahandler.oai.OaiRecord;;
 import dk.kb.util.Resolver;
 import dk.kb.util.xml.XML;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,7 +15,6 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
@@ -31,24 +30,26 @@ import static org.mockito.Mockito.when;
 public class DataEnricherTest {
 
     private FragmentsClient fragmentsClientMock;
+    private ObjectMapper mapper;
 
     @BeforeEach
     public void setUp() throws IOException, URISyntaxException {
         ServiceConfig.initialize("conf/ds-datahandler-behaviour.yaml","conf/ds-datahandler-local.yaml");
 
         fragmentsClientMock = Mockito.mock(FragmentsClient.class);
-
-        String fragmentsString = Files.readString(Resolver.getPathFromClasspath("xml/fragments.json"));
-        ObjectMapper mapper = new ObjectMapper();
-        List<Fragment> fragments = mapper.readValue(fragmentsString, new TypeReference<List<Fragment>>(){});
-        when(fragmentsClientMock.fetchMetadataFragments("test1")).thenReturn(fragments);
+        mapper = new ObjectMapper();
 
         MockedStatic<FragmentsClient> mockedStatic = Mockito.mockStatic(FragmentsClient.class);
         mockedStatic.when(FragmentsClient::getInstance).thenReturn(fragmentsClientMock);
     }
 
     @Test
-    public void testEnrichment() throws IOException, URISyntaxException, ParserConfigurationException, TransformerException, SAXException, XPathExpressionException {
+    public void testEnrichment() throws IOException, ParserConfigurationException, SAXException, XPathExpressionException, URISyntaxException {
+        String fragmentsString = Files.readString(Resolver.getPathFromClasspath("xml/fragments.json"));
+        List<Fragment> fragments = mapper.readValue(fragmentsString, new TypeReference<List<Fragment>>(){});
+        when(fragmentsClientMock.fetchMetadataFragments("test1")).thenReturn(fragments);
+
+
         OaiRecord oaiRecord = new OaiRecord();
         oaiRecord.setId("oai:io:test1");
         oaiRecord.setMetadata(Files.readString(Resolver.getPathFromClasspath("xml/unenriched-metadata-test1.xml")));
@@ -59,6 +60,40 @@ public class DataEnricherTest {
         assertEquals(numberOfmetadatanodesBefore+1,numberOfmetadatanodesAfter);
     }
 
+    @Test
+    public void testEnrichmentEmpty() throws IOException, ParserConfigurationException, SAXException, XPathExpressionException, URISyntaxException {
+        String fragmentsString = Files.readString(Resolver.getPathFromClasspath("xml/fragments-empty.json"));
+        List<Fragment> fragments = mapper.readValue(fragmentsString, new TypeReference<List<Fragment>>(){});
+        when(fragmentsClientMock.fetchMetadataFragments("test2")).thenReturn(fragments);
+
+
+        OaiRecord oaiRecord = new OaiRecord();
+        oaiRecord.setId("oai:io:test2");
+        oaiRecord.setMetadata(Files.readString(Resolver.getPathFromClasspath("xml/unenriched-metadata-test1.xml")));
+
+        int numberOfmetadatanodesBefore = countMetadataNodes(oaiRecord);
+        DataEnricher.apply(oaiRecord);
+        int numberOfmetadatanodesAfter = countMetadataNodes(oaiRecord);
+        assertEquals(numberOfmetadatanodesBefore,numberOfmetadatanodesAfter);
+    }
+
+    @Test
+    public void testEnrichmentMulti() throws IOException, ParserConfigurationException, SAXException, XPathExpressionException, URISyntaxException {
+        String fragmentsString = Files.readString(Resolver.getPathFromClasspath("xml/fragments-multi.json"));
+        List<Fragment> fragments = mapper.readValue(fragmentsString, new TypeReference<List<Fragment>>(){});
+        when(fragmentsClientMock.fetchMetadataFragments("test3")).thenReturn(fragments);
+
+
+        OaiRecord oaiRecord = new OaiRecord();
+        oaiRecord.setId("oai:io:test3");
+        oaiRecord.setMetadata(Files.readString(Resolver.getPathFromClasspath("xml/unenriched-metadata-test1.xml")));
+
+        int numberOfmetadatanodesBefore = countMetadataNodes(oaiRecord);
+        DataEnricher.apply(oaiRecord);
+        int numberOfmetadatanodesAfter = countMetadataNodes(oaiRecord);
+        assertEquals(numberOfmetadatanodesBefore+2,numberOfmetadatanodesAfter);
+    }
+
     private int countMetadataNodes(OaiRecord oaiRecord) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
         XPathFactory xPathFactory = XPathFactory.newInstance();
         XPath xpath = xPathFactory.newXPath();
@@ -66,6 +101,5 @@ public class DataEnricherTest {
         NodeList metadataNodes = (NodeList) xpath.evaluate("/XIP/Metadata", document, XPathConstants.NODESET);
         return metadataNodes.getLength();
     }
-
 
 }
--- src/test/java/dk/kb/datahandler/enrichment/FragmentClientTest.java
+++ src/test/java/dk/kb/datahandler/enrichment/FragmentClientTest.java
@@ -1,19 +1,68 @@
 package dk.kb.datahandler.enrichment;
 
-import dk.kb.storage.model.v1.DsRecordDto;
+import dk.kb.util.Resolver;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 public class FragmentClientTest {
 
+    private FragmentsClient fragmentsClient;
+    private HttpURLConnection mockConnection;
+
+    @BeforeEach
+    public void setUp() throws IOException, URISyntaxException {
+        // Initialize FragmentsClient and mock HttpURLConnection
+        fragmentsClient = Mockito.spy(new FragmentsClient("http://test-url.com",5));
+        mockConnection = mock(HttpURLConnection.class);
+
+        // Mock the getConnection method to always return the mockConnection object
+        doReturn(mockConnection).when(fragmentsClient).getConnection(anyString());
+    }
+
+
+
     @Test
     public void testFetchFragments() throws IOException, URISyntaxException {
-        FragmentsClient client = new FragmentsClient("http://nifi-rtv-stage-node01.kb.dk:9411");
-        DsRecordDto record = new DsRecordDto();
-        record.setId("b8a1a107-59a8-4f74-a6e4-c0026b828d66");
-        client.fetchMetadataFragments(record);
+        when(mockConnection.getResponseCode()).thenReturn(200);
+
+        String jsonResponse = Files.readString(Resolver.getPathFromClasspath("xml/fragments-multi.json"));
+        InputStream jsonStream = new java.io.ByteArrayInputStream(jsonResponse.getBytes());
+        when(mockConnection.getInputStream()).thenReturn(jsonStream);
+
+        List<Fragment> fragments = fragmentsClient.fetchMetadataFragments("test-id");
+        Assertions.assertEquals(2, fragments.size());
+    }
+
+    @Test
+    public void testRetriesOnFailure() throws IOException, URISyntaxException {
+        when(mockConnection.getResponseCode())
+                .thenReturn(500)
+                .thenReturn(500)
+                .thenReturn(200);
+
+        fragmentsClient.fetchMetadataFragments("test");
+        verify(mockConnection, times(3)).getResponseCode();
+    }
+
+    @Test
+    public void testFailure() throws IOException {
+        when(mockConnection.getResponseCode()).thenReturn(500);
+        assertThrows(RuntimeException.class, () -> fragmentsClient.fetchMetadataFragments("test"));
+        verify(mockConnection, times(5)).getResponseCode();
     }
 
 }
