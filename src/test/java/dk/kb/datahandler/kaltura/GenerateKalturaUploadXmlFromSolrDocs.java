c3ea3c145414239f5ea86c91d36039644f5e4974
Thomas Egense <teg@kb.dk>
2024-09-23 13:45:15 +0200

    Added saving xml-files in bundles to a folder

--- src/test/java/dk/kb/datahandler/kaltura/GenerateKalturaUploadXmlFromSolrDocs.java
+++ src/test/java/dk/kb/datahandler/kaltura/GenerateKalturaUploadXmlFromSolrDocs.java
@@ -1,32 +1,66 @@
 package dk.kb.datahandler.kaltura;
 
-import static org.mockito.ArgumentMatchers.refEq;
-
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import com.google.gson.Gson;
 
 import dk.kb.util.Resolver;
 
 
+/**
+ * The purpose of this class is to parse a Solr document from ds-solr with records and make Kaltura XML's files with upload URL to the stream.
+ * The XML files are then upload to Kaltura manuel if there are few or sent to Petur for very large scale uploads.
+ * 
+ * The solr json document can be produced with this query: holdback_expired_date:[NOW TO *] 
+ * 
+ * 
+ * The Kaltura upload XML has the following structure. To see the item XML see the 'kaltura_item.xml' template. 
+ * Change the template if upload format changes or maybe one of the 3 tags are removed 
+ * The number of items in each file must be limited or kaltura will break. The limit is currently sat at 100 items for xml file.
+ * 
+ * <?xml version='1.0' encoding='utf-8'?>
+ * <mrss>
+ *   <channel>
+ *     <item>...</item>
+ *     <item>...</item>
+ *     .... more items
+ *   </channel>
+ * </mrss>   
+ * 
+ * 
+ * The file name of the xml files should follow this syntax. Each file having 100 items.
+ * DRA_2024-07-07_PROD_1.xml
+ * DRA_2024-07-07_PROD_2.xml'
+ * .....
+ * 
+ * 
+ * 
+ * For STAGE replace PROD with STAGE * 
+ * 
+ * Before starting the job, read the constants below and change values. 
+ * 
+ */
 
 public class GenerateKalturaUploadXmlFromSolrDocs {
 
     final static String XML_KALTURA_ITEM_FRAGMENT_FILE="kaltura/kaltura_item.xml"; //Do not change.
-    static String XML_KALTURA_ITEM_FRAGMENT;
+    final static String XML_START="<?xml version='1.0' encoding='utf-8'?>\n<mrss>\n<channel>\n";
+    final static String XML_END="<\\channel>\n<\\mrss>\n";        
     
- 
- 
- 
-        
- 
-    
+    static String XML_KALTURA_ITEM_FRAGMENT; //Not final since it will be loaded once in the main method
+   
+    // The number of item xml blocks in each file
+    final static int NUMBER_ITEMS_IN_EACH=100;
     
     //Kaltura STAGE
     final static int VIDEO_CONVERSION_PROFILE_ID=1406; //This is Kaltura stage for Video
@@ -35,6 +69,12 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
     //Kaltura PROD
     //final static int VIDEO_CONVERSION_PROFILE_ID=? // Ask Asger
     //final static int AUDIO_CONVERSION_PROFILE_ID=? // Ask Asger
+
+    //This will be different for stage?
+    //The KUANA paths (stage) FTP server has not been established
+    final static String FTP_PRESERVICA_RADIOTV_PATH="https://deic-download.kb.dk/radio-tv/";     
+    final static String FTP_BART_TV_PATH="https://deic-download.kb.dk/kuana-store/bart-access-copies-tv/";
+    final static String FTP_BART_RADIO_PATH="https://deic-download.kb.dk/kuana-store/bart-access-copies-radio/";
 
     
     //This should be same on both STAGE and PROD
@@ -46,18 +86,22 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
     final static int MEDIATYPE_AUDIO=5;
         
     final static String TAG1="ds-kaltura"; //This should not be changed
-    final static String TAG2="XML-2024-07T11:27"; //Change this to about current date
-    final static String TAG3="test"; //Asger used this on kaltura-STAGE,
+    final static String TAG2="XML-2024-09-23"; //Change this to about current date
+    final static String TAG3="test"; //Asger used this on kaltura-STAGE. Dont know what to use for PROD.
     
     
     //Custom values that must be changed before running
     final static String SOLR_DOCS_JSON="/home/teg/Desktop/temp/solr_docs.json";
-
+    //The output folder for the xml files
+    final static String OUTPUT_FOLDER="/home/teg/Desktop/temp/";
+    //Fix Date in file before running job.
+    final static String XML_FILE_PATTERN="DRA_2024-9-23_STAGE_#NUMBER.xml";   //Kaltura stage
+    //static String filePattern="DRA_2024-07-07_PROD_#NUMBER.xml";  //Kaltura production
+    
+    
     
     public static void main(String[] args) {
-        
-
-        
+                
         try {
             //Load xml fragment file only once
             XML_KALTURA_ITEM_FRAGMENT = readFile(XML_KALTURA_ITEM_FRAGMENT_FILE);
@@ -65,17 +109,19 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
             //Load the solr json with documents and convert to javaDTO's
             ArrayList<KalturaItemXml> itemXmlList = createKalturaItemsFromSolrJson(SOLR_DOCS_JSON);
             
-            //Split list into sublistof 500
-            List<List<KalturaItemXml>> lists = ListUtils.partition(itemXmlList, 500);
-            
+            //Split list into sublists
+            List<List<KalturaItemXml>> lists = ListUtils.partition(itemXmlList, NUMBER_ITEMS_IN_EACH);
+            int blockCount=1;
+        
+           //generate file for each block 
            for (List<KalturaItemXml> subList : lists) {
-                 generateXmlFromListOfKalturaXML(subList);
-                  
-           }
-            
-                        
-            //String xmlSubstituted= substituteValues(xml, xmlItem);
-            //System.out.println(xmlSubstituted);                        
+               String xmlWithItems = generateXmlFromListOfKalturaXML(subList);                                         
+               String fileName=XML_FILE_PATTERN.replaceFirst("#NUMBER", ""+blockCount++);
+               Path filePath=Paths.get(OUTPUT_FOLDER+fileName);
+               
+               //Save file
+               writeToFile(xmlWithItems, filePath);                                             
+           }                                                                  
         }
         catch(Exception e) {        
             System.out.println("Error creating xml");
@@ -90,16 +136,14 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
     private static String generateXmlFromListOfKalturaXML(List<KalturaItemXml> list) throws Exception {
         StringBuilder xml= new StringBuilder();
         
-        //TODO start xml
+        xml.append(XML_START);
         for (KalturaItemXml kalturaXml : list) {
             String itemXMLString = substituteValues(XML_KALTURA_ITEM_FRAGMENT, kalturaXml);
-            System.out.println(itemXMLString);
-                        
+            xml.append(itemXMLString);
+            xml.append("\n"); 
         }
-        
-        
-        //TODO end xml
-        
+        xml.append(XML_END);
+                
         return xml.toString();
     }
     
@@ -142,14 +186,12 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
                 continue; //Do not add 
             }
                         
-            //TODO download url depends on environment and origin and must be calculated from file_id
-            String downloadUrl="http://test";
+            String downloadUrl=generat(referenceId);
             KalturaItemXml itemXml= new KalturaItemXml(type, referenceId, name, description, tag1, tag2, tag3, conversionProfileId,  type,flavorParamsId, downloadUrl);
             itemXmlList.add(itemXml);                                                  
         }
         
-        return itemXmlList;
-        
+        return itemXmlList;        
     }
     
     /*
@@ -185,21 +227,58 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
         }
     }
     
+    /*
+     * Escape text correct for XML 
+     */
+    private static String xmlEncode(String text) {        
+        return StringEscapeUtils.escapeXml11(text);        
+    }
+    
+
+    /*
+     * Write text to a new file
+     */
+    private static void writeToFile(String text, Path filePath) throws Exception {        
+        Files.writeString(filePath, text, StandardCharsets.UTF_8);        
+        System.out.println("Created file:"+filePath.toAbsolutePath());
+    }
     
     
+    /*
+     * Substitute values in the xml template. Use XML encode for strings that can have special characters.
+     */
     private static String substituteValues(String xml, KalturaItemXml xmlItem) {        
-        xml=xml.replaceFirst("#TYPE", ""+xmlItem.getType());
-        xml=xml.replaceFirst("#REFERENCE_ID", xmlItem.getReferenceId());
-        xml=xml.replaceFirst("#NAME", xmlItem.getName());
-        xml=xml.replaceFirst("#DESCRIPTION", xmlItem.getDescription());
-        xml=xml.replaceFirst("#TAG1", xmlItem.getTag1());
-        xml=xml.replaceFirst("#TAG2", xmlItem.getTag2());
-        xml=xml.replaceFirst("#TAG3", xmlItem.getTag3());
+        xml=xml.replaceFirst("#TYPE",""+xmlItem.getType());
+        xml=xml.replaceFirst("#REFERENCE_ID",  xmlEncode(xmlItem.getReferenceId()));
+        xml=xml.replaceFirst("#NAME",  xmlEncode(xmlItem.getName()));
+        xml=xml.replaceFirst("#DESCRIPTION",  xmlEncode(xmlItem.getDescription()));
+        xml=xml.replaceFirst("#TAG1",  xmlEncode(xmlItem.getTag1()));
+        xml=xml.replaceFirst("#TAG2",  xmlEncode(xmlItem.getTag2()));
+        xml=xml.replaceFirst("#TAG3",  xmlEncode(xmlItem.getTag3()));
         xml=xml.replaceFirst("#CONVERSION_PROFILE_ID", ""+xmlItem.getConversionProfileId());
         xml=xml.replaceFirst("#MEDIATYPE", ""+xmlItem.getMediaType());
         xml=xml.replaceFirst("#FLAVOR_PARAMS_ID",""+xmlItem.getFlavorParamsId());
-        xml=xml.replaceFirst("#DOWNLOAD_URL", ""+xmlItem.getDownloadUrl());
+        xml=xml.replaceFirst("#DOWNLOAD_URL", ""+ xmlEncode(xmlItem.getDownloadUrl()));
         return xml;
     }
-
+    
+    
+    //Example: https://deic-download.kb.dk/radio-tv/e/b/4/f/eb4fcb8c-99e3-415b-8cc8-33a6ffb17b73.mp4
+    //TODO. Is it DOMS-radio,DOMS-tv or preservica??
+    private static String generatePreservicaTvDownloadUrl(String fileId) {           
+        String pathSplit= fileId.substring(0,2)+"/"+fileId.substring(2,4)+"/"+fileId.substring(4,6)+"/"+fileId;       
+         return FTP_KUANA_TV_PATH+pathSplit;               
+    }
+    
+    private static String generateKuanaRadioDownloadUrl(String fileId) {           
+        String pathSplit= fileId.substring(0,2)+"/"+fileId.substring(2,4)+"/"+fileId.substring(4,6)+"/"+fileId;       
+         return FTP_KUANA_RADIO_PATH+pathSplit;               
+    }
+    
+    private static String generateKuanaTVDownloadUrl(String fileId) {           
+        String pathSplit= fileId.substring(0,2)+"/"+fileId.substring(2,4)+"/"+fileId.substring(4,6)+"/"+fileId;       
+         return FTP_KUANA_TV_PATH+pathSplit;               
+    }
+    
+    
 }
