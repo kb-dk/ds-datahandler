a9310de1dea9f8f773a2663dc3705a0325292da5
Thomas Egense <teg@kb.dk>
2024-09-24 11:20:36 +0200

    Finished work

--- src/test/java/dk/kb/datahandler/kaltura/GenerateKalturaUploadXmlFromSolrDocs.java
+++ src/test/java/dk/kb/datahandler/kaltura/GenerateKalturaUploadXmlFromSolrDocs.java
@@ -58,35 +58,35 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
     final static String XML_KALTURA_ITEM_FRAGMENT_FILE="kaltura/kaltura_item.xml"; //Do not change.
     final static String XML_START="<?xml version='1.0' encoding='utf-8'?>\n<mrss>\n<channel>\n";
     final static String XML_END="<\\channel>\n<\\mrss>\n";        
-    
+
     static String XML_KALTURA_ITEM_FRAGMENT; //Not final since it will be loaded once in the main method
-   
+
     // The number of item xml blocks in each file. This is a limit Kaltura like.
     final static int NUMBER_ITEMS_IN_EACH=100;
-    
+
     //Change for STAGE or PROD! (I do not know PROD values yet)
     final static int VIDEO_CONVERSION_PROFILE_ID=1406; //This is Kaltura stage for Video
     final static int AUDIO_CONVERSION_PROFILE_ID=1403; //This is Kaltura stage for Audio
-    
+
     //The KUANA paths (stage) FTP server has not been established
     //Name for stage will probably be: deic-download-stage.kb.dk
     final static String FTP_PRESERVICA_RADIOTV_PATH="https://deic-download.kb.dk/radio-tv/";     
     final static String FTP_BART_TV_PATH="https://deic-download.kb.dk/kuana-store/bart-access-copies-tv/";
     final static String FTP_BART_RADIO_PATH="https://deic-download.kb.dk/kuana-store/bart-access-copies-radio/";
-    
+
     //This should be same on both STAGE and PROD
     final static int VIDEO_FLAVOR_PARAMS_ID=3;
     final static int AUDIO_FLAVOR_PARAMS_ID=359;
-        
+
     //Kaltura enums. Same on stage/prod
     final static int MEDIATYPE_VIDEO=1;
     final static int MEDIATYPE_AUDIO=5;
-        
+
     final static String TAG1="ds-kaltura"; //This should not be changed
     final static String TAG2="XML-2024-09-23"; //Change this to about current date
     final static String TAG3="test"; //Asger used this on kaltura-STAGE. Dont know what to use for PROD.
-    
-    
+
+
     //Custom values that must be changed before running
     final static String SOLR_DOCS_JSON="/home/teg/Desktop/temp/solr_preservica_audio.json";
     //The output folder for the xml files
@@ -94,32 +94,32 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
     //Fix Date in file before running job.
     final static String XML_FILE_PATTERN="DRA_2024-9-23_STAGE_#NUMBER.xml";   //Kaltura stage
     //static String filePattern="DRA_2024-07-07_PROD_#NUMBER.xml";  //Kaltura production
-    
-    
-    
+
+
+
     public static void main(String[] args) {
-                
+
         try {
             //Load xml fragment file only once
             XML_KALTURA_ITEM_FRAGMENT = readFile(XML_KALTURA_ITEM_FRAGMENT_FILE);
-                        
+
             //Load the solr json with documents and convert to javaDTO's
             ArrayList<KalturaItemXml> itemXmlList = createKalturaItemsFromSolrJson(SOLR_DOCS_JSON);
             System.out.println("Solr document has #docs="+itemXmlList.size());
-            
+
             //Split list into sublists
             List<List<KalturaItemXml>> lists = ListUtils.partition(itemXmlList, NUMBER_ITEMS_IN_EACH);
             int blockCount=1;
-        
-           //generate file for each block 
-           for (List<KalturaItemXml> subList : lists) {
-               String xmlWithItems = generateXmlFromListOfKalturaXML(subList);                                         
-               String fileName=XML_FILE_PATTERN.replaceFirst("#NUMBER", ""+blockCount++);
-               Path filePath=Paths.get(OUTPUT_FOLDER+fileName);
-               
-               //Save file
-               writeToFile(xmlWithItems, filePath);                                             
-           }                                                                  
+
+            //generate file for each block 
+            for (List<KalturaItemXml> subList : lists) {
+                String xmlWithItems = generateXmlFromListOfKalturaXML(subList);                                         
+                String fileName=XML_FILE_PATTERN.replaceFirst("#NUMBER", ""+blockCount++);
+                Path filePath=Paths.get(OUTPUT_FOLDER+fileName);
+
+                //Save file
+                writeToFile(xmlWithItems, filePath);                                             
+            }                                                                  
         }
         catch(Exception e) {        
             System.out.println("Error creating xml");
@@ -127,10 +127,10 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
 
         }
     }
-   
+
     private static String generateXmlFromListOfKalturaXML(List<KalturaItemXml> list) throws Exception {
         StringBuilder xml= new StringBuilder();
-        
+
         xml.append(XML_START);
         for (KalturaItemXml kalturaXml : list) {
             String itemXMLString = substituteValues(XML_KALTURA_ITEM_FRAGMENT, kalturaXml);
@@ -138,23 +138,23 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
             xml.append("\n"); 
         }
         xml.append(XML_END);
-                
+
         return xml.toString();
     }
-    
+
     private static ArrayList<KalturaItemXml> createKalturaItemsFromSolrJson(String jsonFile) throws Exception{
-                                
+
         String solrJson= readFile(jsonFile);;
-        
+
         JSONObject solrResponse = new JSONObject(solrJson);
         JSONArray json_docs = solrResponse.getJSONObject("response").getJSONArray("docs");
-        
+
         ArrayList<KalturaItemXml> itemXmlList = new ArrayList<KalturaItemXml>(); 
-        
+
         for (int i=0;i< json_docs.length();i++) {
-            
+
             JSONObject doc = (JSONObject) json_docs.get(i);
-            
+
             String referenceId= doc.getString("file_id");            
             String name = getFirstTitle(doc);
             String description = getFieldOrEmpty(doc,"description");
@@ -165,7 +165,7 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
             int type;  //Will be different for video/audio
             int conversionProfileId; //Will be different for video/audio
             int flavorParamsId; //Will be different for video/audio
-            
+
             String resourceType=doc.getString("resource_description");
             if ("VideoObject".equals(resourceType)) {                
                 type=MEDIATYPE_VIDEO;
@@ -181,16 +181,16 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
                 System.out.println("Unknown resource_description:"+resourceType);
                 continue; //Do not add 
             }
-                  
-            
+
+
             String downloadUrl=generateDownloadUrl(migratedFrom,resourceType,referenceId);
             KalturaItemXml itemXml= new KalturaItemXml(type, referenceId, name, description, migratedFrom, tag1, tag2, tag3, conversionProfileId,  type,flavorParamsId, downloadUrl);
             itemXmlList.add(itemXml);                                                  
         }
-        
+
         return itemXmlList;        
     }
-    
+
     /*
      * Read a text file to string.  
      */
@@ -199,38 +199,38 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
         return text;
     }
 
-     
+
     // Return first title value since it is multivalued. All documents should have at least 1 title.
     private static String getFirstTitle(JSONObject doc ) {
         try {
-         JSONArray titleArray = doc.getJSONArray("title"); //title field is multivalued
-         return titleArray.get(0).toString(); //First
+            JSONArray titleArray = doc.getJSONArray("title"); //title field is multivalued
+            return titleArray.get(0).toString(); //First
         }
         catch(Exception e) {            
             System.out.println("No title found for id:"+doc.getString("ID"));
             return "";
-            
+
         }
     }
-    
-    
+
+
     //Return empty string if field does not exist
     private static String getFieldOrEmpty(JSONObject doc, String field ) {
         try {
-           return doc.getString(field);
+            return doc.getString(field);
         }
         catch(Exception e) {                      
             return "";            
         }
     }
-    
+
     /*
      * Escape text correct for XML 
      */
     private static String xmlEncode(String text) {        
         return StringEscapeUtils.escapeXml11(text);        
     }
-    
+
 
     /*
      * Write text to a new file
@@ -239,8 +239,8 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
         Files.writeString(filePath, text, StandardCharsets.UTF_8);        
         System.out.println("Created file:"+filePath.toAbsolutePath());
     }
-    
-    
+
+
     /*
      * Substitute values in the xml template. Use XML encode for strings that can have special characters.
      */
@@ -258,10 +258,10 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
         xml=xml.replaceFirst("#DOWNLOAD_URL", ""+ xmlEncode(xmlItem.getDownloadUrl()));
         return xml;
     }
-    
-    
-    
-    
+
+
+
+
     //File path examples. Notice there is no extension for bart-tv and bart-radio
     //Example Preservica/Kuana: https://deic-download.kb.dk/radio-tv/e/b/4/f/eb4fcb8c-99e3-415b-8cc8-33a6ffb17b73.mp4
     //Example bart-tv:   https://deic-download.kb.dk/kuana-store/bart-access-copies-tv/00/01/32
@@ -270,32 +270,32 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
     private static String generateDownloadUrl(String migratedFrom, String resourceType,String referenceId) {
 
         if ("DOMS".equals(migratedFrom)){ //This is bart
-          if ("VideoObject".equals(resourceType)) {              
-              return generateBartTvDownloadUrl(referenceId);
+            if ("VideoObject".equals(resourceType)) {              
+                return generateBartTvDownloadUrl(referenceId);
 
-          }
-          else {              
-              return generateBartRadioDownloadUrl(referenceId);
-          }          
+            }
+            else {              
+                return generateBartRadioDownloadUrl(referenceId);
+            }          
         }
         else {
-          return generateKuanaTVDownloadUrl(referenceId, resourceType);              
+            return generateKuanaTVDownloadUrl(referenceId, resourceType);              
         }        
     }
-    
-    
+
+
     // 2  character folders
     private static String generateBartTvDownloadUrl(String fileId) {           
         String pathSplit= fileId.substring(0,2)+"/"+fileId.substring(2,4)+"/"+fileId.substring(4,6)+"/"+fileId;       
-         return FTP_BART_TV_PATH+pathSplit;     
+        return FTP_BART_TV_PATH+pathSplit;     
     }
-    
+
     // 2  character folders
     private static String generateBartRadioDownloadUrl(String fileId) {           
         String pathSplit= fileId.substring(0,2)+"/"+fileId.substring(2,4)+"/"+fileId.substring(4,6)+"/"+fileId;       
-         return FTP_BART_RADIO_PATH+pathSplit;               
+        return FTP_BART_RADIO_PATH+pathSplit;               
     }
-    
+
     // 1  character folders + add extension .mp3 or .mp4
     private static String generateKuanaTVDownloadUrl(String fileId,String resourceType) {           
         String pathSplit= fileId.substring(0,1)+"/"+fileId.substring(1,2)+"/"+fileId.substring(2,3)+"/"+fileId;       
@@ -307,5 +307,5 @@ public class GenerateKalturaUploadXmlFromSolrDocs {
             return fileWithOutExtension+".mp3";
         }            
     }
-        
+
 }
