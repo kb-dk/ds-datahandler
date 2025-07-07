9f805e234570ff40a8ad4010a877134da5340977
David Grove Jørgensen <dgj@kb.dk>
2025-07-07 14:57:23 +0200

    get unittest config from yaml-file

--- src/test/java/dk/kb/datahandler/facade/DsDatahandlerFacadeTest.java
+++ src/test/java/dk/kb/datahandler/facade/DsDatahandlerFacadeTest.java
@@ -4,23 +4,38 @@ package dk.kb.datahandler.facade;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import dk.kb.datahandler.config.ServiceConfig;
 import dk.kb.datahandler.model.v1.*;
 import dk.kb.datahandler.storage.BasicStorage;
 import dk.kb.datahandler.storage.JobStorage;
+import dk.kb.datahandler.util.H2DbUtil;
+import dk.kb.util.webservice.exception.InvalidArgumentServiceException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class DsDatahandlerFacadeTest {
+    protected static final String TEST_CLASSES_PATH = new File(Thread.currentThread().getContextClassLoader().getResource("logback-test.xml").getPath()).getParentFile().getAbsolutePath();
+    protected static final String DB_URL = "jdbc:h2:"+TEST_CLASSES_PATH+"/h2/ds_datahandler;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE";
+    private static final String DRIVER = "org.h2.Driver";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+    private static JobStorage storage = null;
+
+    @BeforeAll
+    public static void beforeClass() throws Exception {
+        ServiceConfig.initialize("ds-datahandler-unittest.yaml");
+        H2DbUtil.createEmptyH2DBFromDDL(DB_URL, DRIVER, USERNAME, PASSWORD);
+        JobStorage.initialize(DRIVER, DB_URL, USERNAME, PASSWORD);
+        storage = new JobStorage();
+    }
 
     @Test
     void testOnly1JobWithSameName() throws Exception {
         
-        OaiTargetDto oaiTarget = new OaiTargetDto()
-                .url("http://www5.kb.dk/cop/oai/")
-                .metadataprefix("mods")
-                .set("oai:kb.dk:images:billed:2010:okt:billeder")
-                .username(null).password(null)
-                .datasource("test")
-                .name("THERE_CAN_ONLY_BE_ONE");
+        OaiTargetDto oaiTarget = ServiceConfig.getOaiTargets().get("test.target");
         
         // OK
         JobDto jobDto = new JobDto();
@@ -34,12 +49,9 @@ public class DsDatahandlerFacadeTest {
         });
 
         // Try add another
-        try {
-            DsDatahandlerFacade.oaiIngestFull(oaiTarget.getName(),"unit test");
-            fail("Two jobs with same name was started");
-        } catch (Exception e) {
-            // ignore
-        }
+        Assertions.assertThrows(InvalidArgumentServiceException.class,
+                () -> DsDatahandlerFacade.oaiIngestFull(oaiTarget.getName(),"unit test")
+        );
 
     }
 
