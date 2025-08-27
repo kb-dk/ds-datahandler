d10a000da1a58b6de97afd2b7fac1831554fd814
kesj <kesj@kb.dk>
2025-08-27 13:58:13 +0200

    Fix to handle ApiException

--- src/main/java/dk/kb/datahandler/kaltura/KalturaDeltaUploadJob.java
+++ src/main/java/dk/kb/datahandler/kaltura/KalturaDeltaUploadJob.java
@@ -252,9 +252,9 @@ public class KalturaDeltaUploadJob {
      * This method is not called by the upload flow, but from the integration unittest. 
      * 
      * @param  kalturaEntryId  The internal kaltura entryId
-     * @throws IOException If API error
+     * @throws APIException If API error
      */
-    public static void deleteStream(String kalturaEntryId) throws IOException {
+    public static void deleteStream(String kalturaEntryId) throws APIException {
         initKalturaClient();            
         boolean deleteStreamByEntryId = kalturaClient.deleteStreamByEntryId( kalturaEntryId);
         if (deleteStreamByEntryId) {
@@ -272,9 +272,10 @@ public class KalturaDeltaUploadJob {
      * @param file_id Our reference to the stream.
      * @return kalturaId or null if does not exist.
      * @throws IOException
-     * 
+     * @throws APIException If API error
+     *
      */
-    private static String getInternalIdKaltura(String file_id) throws IOException {
+    private static String getInternalIdKaltura(String file_id) throws IOException, APIException {
 
         initKalturaClient();
 
